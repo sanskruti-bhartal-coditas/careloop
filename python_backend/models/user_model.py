deb2662ece0c8c293d6a835878f56f70feec7bd6
Suyog Dhage <suyog.dhage@coditas.com>
2026-07-13 15:04:05 +0530

    intial comit from the fastapi

---
+++ python_backend/models/user_model.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, DateTime,func,Enum
+from sqlalchemy.dialects.postgresql import UUID
+from dependencies.session import Base
+import uuid
+import enum
+
+class Role(str,enum.Enum):
+    patient="patient"
+    coordinator="coordinator"
+    admin="admin"
+
+class Users(Base):
+    __tablename__="users"
+    id=Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    email=Column(String,unique=True,nullable=False)
+    first_name=Column(String,nullable=False)
+    last_name=Column(String,nullable=False)
+    role_name=Column(Enum(Role),nullable=False,default=Role.patient)
+    created_at=Column(DateTime(timezone=True),server_default=func.now())
