f65bbcdc48dee46a1a2c552b06e33f25ccfccf14
sanskruti.bhartal@coditas <sanskruti.bhartal@coditas.com>
2026-07-14 10:08:42 +0530

    configured redux slices and store, configured access and refresh token management

---
+++ frontend/src/services/app.services.ts
@@ -0,0 +1,57 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { getRefreshToken, clearTokens, setTokens, getAccessToken } from "../utils/tokens.utils";
+import { logout } from "../redux/slices/authSlice";
+
+export const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
+  prepareHeaders: (headers) => {
+    const token = getAccessToken();
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+interface RefreshResultInterface {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, store, extraOptions) => {
+
+  let result = await baseQuery(args, store, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    const refreshToken = getRefreshToken();
+
+    if (refreshToken) {
+      const refreshResult = await baseQuery(
+        {
+          url: 'auth/refresh',
+          method: 'POST',
+          body: { refreshToken: refreshToken },
+        },
+        store,
+        extraOptions
+      );
+
+      // if refresh token is valid then it will create .data property 
+      if (refreshResult.data) {
+        const refreshData = refreshResult.data as RefreshResultInterface;
+        setTokens(refreshData.accessToken, refreshData.refreshToken);
+        result = await baseQuery(args, store, extraOptions);
+      } else {
+        // if refresh token is expired then it will create .error property 
+        clearTokens();
+        store.dispatch(logout());
+      }
+    } else {
+      clearTokens();
+      store.dispatch(logout());
+    }
+
+  }
+  return result;
+};
