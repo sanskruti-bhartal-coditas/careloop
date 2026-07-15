374a7d88a1310c536c368b43ee5717d30465fdca
mikhelpottella-coditas <mikhel.pottella@coditas.com>
2026-07-15 13:08:57 +0530

    working on the branhc

--- JavaBackend/src/main/java/org/project/javabackend/config/CustomCorsConfiguration.java
+++ JavaBackend/src/main/java/org/project/javabackend/config/CustomCorsConfiguration.java
@@ -1,11 +1,12 @@
 package org.project.javabackend.config;
 
-import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.List;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Component
 public class CustomCorsConfiguration implements CorsConfigurationSource {
@@ -13,6 +14,10 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("*"));
+<<<<<<< HEAD
+=======
+        config.setAllowCredentials(true);
+>>>>>>> ae2d88d (made chages for cors config)
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         return config;
